ad4a255147445d374668579e44f1408e64516d5d
DanilKrasavcik <dkrasavcik91@gmail.com>
2023-07-22 11:11:15 +0300

    answer2

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -3,15 +3,10 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UsersService;
-
 import java.security.Principal;
-import java.util.HashSet;
-
-import java.util.Set;
 
 
 @Controller
@@ -33,7 +28,6 @@ public class AdminController {
         model.addAttribute("roles", roleService.getRoles());
         model.addAttribute("users", usersService.findAll());
         model.addAttribute("usingUser", usersService.getUserByUsername(principal.getName()));
-        model.addAttribute("allRoles", roleService.getRoles());
         return "admin";
     }
 
@@ -42,7 +36,6 @@ public class AdminController {
     @GetMapping("/new")
     public String newUser(Model model) {
         model.addAttribute("user", new User());
-        model.addAttribute("allRoles", roleService.getRoles());
         return "/new";
     }
 
@@ -56,22 +49,12 @@ public class AdminController {
     @GetMapping("/edit/{id}")
     public String edit(Model model, @PathVariable("id") long id) {
         model.addAttribute("user", usersService.findOne(id));
-        model.addAttribute("roles", roleService.getRoles());
-        model.addAttribute("allRoles", roleService.getRoles());
         return "redirect:/admin";
     }
 
     @PatchMapping("/update/{id}")
     public String update(@ModelAttribute("user") User user,
-                         @ModelAttribute("nameRole") String name,
-                         @PathVariable("id") long id, Model model) {
-
-
-        model.addAttribute("allRoles", roleService.getRoles());
-        Set<Role> roleSet = new HashSet<>();
-
-        roleSet.add(roleService.getRoleByName(name));
-        user.setRoles(roleSet);
+                         @PathVariable("id") long id) {
 
         usersService.update(id, user);
         return "redirect:/admin";
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,30 +1,22 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UsersService;
-
 import java.security.Principal;
 
 @Controller
 public class UserController {
 
     private UsersService usersService;
-    private final RoleService roleService;
 
-    public UserController(UsersService usersService, RoleService roleService) {
+    public UserController(UsersService usersService) {
         this.usersService = usersService;
-        this.roleService = roleService;
     }
 
     @GetMapping("/user")
-    public String getUsers(@AuthenticationPrincipal User user, Model model,
-                           Principal principal) {
-        model.addAttribute("user", user);
+    public String getUsers(Model model,  Principal principal) {
         model.addAttribute("usingUser", usersService.getUserByUsername(principal.getName()));
         return "user";
     }
